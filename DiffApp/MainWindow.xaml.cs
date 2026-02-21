b2da4b4c67f8a7cdc55b51e7307486cc77984aab
Németh Gábor <60961939+nemethgaborradnoti@users.noreply.github.com>
2026-02-21 22:12:18 +0100

    Buttons and cleanup

--- DiffApp/MainWindow.xaml.cs
+++ DiffApp/MainWindow.xaml.cs
@@ -2,20 +2,10 @@
 {
     public partial class MainWindow : Window
     {
-        private readonly IScrollService _scrollService;
-
-        public MainWindow(MainViewModel viewModel, IScrollService scrollService)
+        public MainWindow(MainViewModel viewModel)
         {
             InitializeComponent();
             DataContext = viewModel;
-            _scrollService = scrollService;
-
-            this.Loaded += MainWindow_Loaded;
-        }
-
-        private void MainWindow_Loaded(object sender, RoutedEventArgs e)
-        {
-            _scrollService.RegisterTargets(MainScrollViewer, InputSection);
         }
     }
 }
