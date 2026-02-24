6bec99b6df7ed58544da774c2ee303a7836f019b
Németh Gábor <60961939+nemethgaborradnoti@users.noreply.github.com>
2026-02-24 21:20:26 +0100

    Converter refactoring

--- DiffApp/Converters/MultiValueToArrayConverter.cs
+++ DiffApp/Converters/MultiValueToArrayConverter.cs
@@ -2,15 +2,11 @@
 
 namespace DiffApp.Converters
 {
-    public class MergeParamsConverter : IMultiValueConverter
+    public class MultiValueToArrayConverter : IMultiValueConverter
     {
         public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
         {
-            if (values.Length == 2 && values[0] is ChangeBlock block && values[1] is MergeDirection direction)
-            {
-                return new object[] { block, direction };
-            }
-            return null;
+            return values.Clone();
         }
 
         public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
