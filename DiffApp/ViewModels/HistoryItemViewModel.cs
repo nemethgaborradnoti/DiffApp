218da2de366546e24d017c53afa3b210d42b95aa
Németh Gábor <60961939+nemethgaborradnoti@users.noreply.github.com>
2026-02-25 21:15:50 +0100

    History and Minimap

--- DiffApp/ViewModels/HistoryItemViewModel.cs
+++ DiffApp/ViewModels/HistoryItemViewModel.cs
@@ -1,4 +1,7 @@
-﻿namespace DiffApp.ViewModels
+﻿using DiffApp.Models;
+using System.Text;
+
+namespace DiffApp.ViewModels
 {
     public class HistoryItemViewModel : ViewModelBase
     {
@@ -11,6 +14,9 @@
 
         public string DisplayOriginal => _model.OriginalText;
         public string DisplayModified => _model.ModifiedText;
+
+        public string OriginalPreview => GeneratePreview(OriginalFull);
+        public string ModifiedPreview => GeneratePreview(ModifiedFull);
 
         private string _relativeTime;
         public string RelativeTime
@@ -35,6 +41,7 @@
         public HistoryItemViewModel(DiffHistoryItem model)
         {
             _model = model;
+            _relativeTime = string.Empty;
             UpdateTimeDisplay();
         }
 
@@ -67,6 +74,27 @@
             {
                 RelativeTime = CreatedAt.ToShortDateString();
             }
+        }
+
+        private string GeneratePreview(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+                return string.Empty;
+
+            var lines = text.Replace("\r\n", "\n").Split('\n');
+            var sb = new StringBuilder();
+            int lineCount = 0;
+
+            foreach (var line in lines)
+            {
+                if (lineCount >= 3)
+                    break;
+
+                sb.AppendLine(line);
+                lineCount++;
+            }
+
+            return sb.ToString().TrimEnd();
         }
     }
 }
